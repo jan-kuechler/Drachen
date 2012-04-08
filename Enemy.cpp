ecf1472a937bf510b9d56d0ffc0413cd46e5aa3d
Jan <jan@kuechler-wuppertal.de>
2012-04-08 17:50:31 +0200

    Replaced the find neighbor for-switch loop in Enemy::FindPath by a dedicated function

--- Enemy.cpp
+++ Enemy.cpp
@@ -40,7 +40,7 @@ struct Node
 
 	Node *parent;
 
-	Node(Vector2i pos)
+	explicit Node(Vector2i pos)
 	: x(pos.x), y(pos.y), g(.0f), f(.0f), parent(0)
 	{ }
 
@@ -73,11 +73,11 @@ struct NodePtrComp
 	}
 };
 
-struct NodeEq
+struct NodeEqPos
 {
 	const Node& ref;
 
-	NodeEq(const Node& r)
+	explicit NodeEqPos(const Node& r)
 	: ref(r)
 	{ }
 
@@ -87,13 +87,36 @@ struct NodeEq
 	}
 };
 
+static bool ValidNeighbor(int x, int y, const Map& map)
+{
+	return (x >= 0) && (y >= 0)
+		&& (static_cast<size_t>(x) < map.GetWidth()) && (static_cast<size_t>(y) < map.GetHeight()) 
+		&& map.GetGrid()[x][y];
+}
+
+static void FillNeighbors(std::vector<Vector2i>& c, const Map& map, int x, int y)
+{
+	if (ValidNeighbor(x-1, y, map))
+		c.push_back(Vector2i(x-1, y));
+
+	if (ValidNeighbor(x+1, y, map))
+		c.push_back(Vector2i(x+1, y));
+
+	if (ValidNeighbor(x, y-1, map))
+		c.push_back(Vector2i(x, y-1));
+
+	if (ValidNeighbor(x, y+1, map))
+		c.push_back(Vector2i(x, y+1));
+}
+
 void Enemy::FindPath(size_t tgtX, size_t tgtY)
 {
+	// clear the path stack
 	while (!path.empty())
 		path.pop();
 
 	NodePtrComp comp;
-	std::vector<Node*> allNodes;
+	std::vector<Node*> allNodes; // remember all nodes to free them at the end
 	std::vector<Node*> open, closed;
 
 	Node *goal = new Node(tgtX, tgtY);
@@ -114,37 +137,19 @@ void Enemy::FindPath(size_t tgtX, size_t tgtY)
 		open.pop_back();
 
 		// and find all neighbors
-		// TODO: cleanup the for-switch mess!
-		for (int i=0; i < 4; ++i) {
-			int x, y;
-			switch (i) {
-			case 0:
-				x = cur->x-1; y = cur->y;
-				break;
-			case 1:
-				x = cur->x+1; y = cur->y;
-				break;
-			case 2: 
-				x = cur->x; y = cur->y-1;
-				break;
-			case 3:
-				x = cur->x; y = cur->y+1;
-				break;
-			}
-			if (x < 0 || y < 0 || (size_t)x >= map->GetWidth() || (size_t)y >= map->GetHeight() || (x == cur->x && y == cur->y) || !map->GetGrid()[x][y])
-				continue;
+		std::vector<Vector2i> neighbors;
+		FillNeighbors(neighbors, *map, cur->x, cur->y);
 
+		for (std::vector<Vector2i>::iterator it = neighbors.begin(); it != neighbors.end(); ++it) {
 			// neighbor node
-			Node *suc = new Node(x, y);
+			Node *suc = new Node(it->x, it->y);
 			allNodes.push_back(suc);
 
 			float newg = cur->g + 1.f; // the cost to reach the neighbor is the cost to reach the current node plus one step
 
-			std::vector<Node*>::iterator oit, cit;
-
 			// find the neighbor in the open or the closed list
-			oit = boost::find_if(open, NodeEq(*suc));
-			cit = boost::find_if(closed, NodeEq(*suc));
+			std::vector<Node*>::iterator oit = boost::find_if(open, NodeEqPos(*suc));
+			std::vector<Node*>::iterator cit = boost::find_if(closed, NodeEqPos(*suc));
 
 			if (oit != open.end()) {
 				if ((*oit)->g <= newg)
@@ -155,10 +160,12 @@ void Enemy::FindPath(size_t tgtX, size_t tgtY)
 					continue;
 			}
 
+			// best step so far
 			suc->parent = cur;
 			suc->g = newg;
 			suc->f = suc->g + suc->CalcH(*goal);
 
+			// remove the node from open/closed
 			if (cit != closed.end()) {
 				closed.erase(cit);
 			}
