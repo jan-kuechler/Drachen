2e025651ff4718d8e22ef1d937871a4c4abc8d7a
Jan <jan@kuechler-wuppertal.de>
2013-03-17 16:01:24 +0100

    Added prelimiary canon tower definition

--- data/themes/default/towers.js
+++ data/themes/default/towers.js
@@ -30,6 +30,37 @@
 					"power": 4
 				}
 			]
+		},
+		{
+			"name" : "Canon Tower",
+			"type" : "canon",
+			"base-cost": 100,
+			"stages" : [
+				{
+					"base": "Gun_level1.png",
+                    "projectile": "arrow.png",
+					"range": 100,
+					"cooldown": 1.0,
+					"attacks": 1,
+					"power": 2
+				},
+				{
+					"base": "Gun_level2.png",
+                    "projectile": "arrow.png",
+					"range": 120,
+					"cooldown": 0.9,
+					"attacks": 2,
+					"power": 3
+				},
+				{
+					"base": "Gun_level3.png",
+                    "projectile": "arrow.png",
+					"range": 150,
+					"cooldown": 0.8,
+					"attacks": 3,
+					"power": 4
+				}
+			]
 		}
 	]
 }
