58c3b92571cccc55eaa34944424ed82108ed2df4
Jan <kuchen_@gmx.de>
2013-03-28 21:53:34 +0100

    Add screenshot function (P key)

--- Utility.cpp
+++ Utility.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 #include "Utility.h"
 
+namespace fs = boost::filesystem;
+
+void MakeScreenshot(RenderWindow& win);
+
 bool DefaultHandleEvent(RenderWindow& win, Event& event)
 {
 	switch (event.Type) {
@@ -13,8 +17,32 @@ bool DefaultHandleEvent(RenderWindow& win, Event& event)
 			win.Close();
 			return true;
 		}
+		else if (event.Key.Code == Key::P) {
+			MakeScreenshot(win);
+		}
 
 	default:
 		return false;
 	}
+}
+
+fs::path GetScreenshotPath()
+{
+	fs::path def = "screenshot.png";
+	if (!fs::exists(def))
+		return def;
+
+	size_t i=0;
+	for (size_t i=1; i < 10000; ++i) {
+		fs::path p = "screenshot-" + boost::lexical_cast<std::string>(i) + ".png";
+		if (!fs::exists(p))
+			return p;
+	}
+
+	return def;
+}
+
+void MakeScreenshot(RenderWindow& win)
+{
+	win.Capture().SaveToFile(GetScreenshotPath().string());
 }
